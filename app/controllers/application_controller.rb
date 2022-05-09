4086a3889e9b7d4e745ada6e770b7d030da979c9
Rahaf Alhabashi <rahafalhabashi@gmail.com>
2022-05-09 17:18:38 -0400

    commit tree 81769e373d485453c6af82b103ccae28b0045bca

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,6 @@
+class ApplicationController < Sinatra::Base 
+
+    get '/' do
+        '<h2>Hello <em>Worlddddd</em>, I\'ve missed you!</h2>'
+    end
+end
--- spec/02_controllers/application_controller_spec.rb
+++ spec/02_controllers/application_controller_spec.rb
@@ -1,9 +1,7 @@
 describe ApplicationController do
-
   describe "GET /" do
-    it 'returns a valid response' do
-      expect { get '/' }.not_to raise_error
+    it "returns a valid response" do
+      expect { get "/" }.not_to raise_error
     end
   end
-
 end
